c653b8285711d6fba32e494054738974fe6a942f
Behrang <behrang.mahjani@gmail.com>
2019-12-08 16:37:44 -0500

    Create 2.1.7.R

---
+++ 2.1.7.R
@@ -0,0 +1,10 @@
+### Importance sampling, Example 2.3
+meth2 <- function(N)
+{
+y <- rexp(N) ### trial distribution Exp(1)
+w <- y^2*3*exp(-y^3+y) ### weights
+h <- (y^3*4/3)[y<0.4]
+W <- W[y<0.4]
+return(sum(h*W)/sum(w)) ### integral value
+}
+meth2(1000) ### carry out the method
