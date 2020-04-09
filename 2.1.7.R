f6d6ea6d1ed5d560d2af905990adff9dc49e393c
Claus Ekstrøm <github@ekstroem.com>
2020-04-09 12:03:16 +0200

    Update 2.1.7.R
    Fixed two errors in the code so it actually runs without errors and produces the right result

--- 2.1.7.R
+++ 2.1.7.R
@@ -3,8 +3,8 @@ meth2 <- function(N)
 {
 y <- rexp(N) ### trial distribution Exp(1)
 w <- y^2*3*exp(-y^3+y) ### weights
-h <- (y^3*4/3)[y<0.4]
-W <- W[y<0.4]
+h <- (y*4/3)[y<0.4]
+W <- w[y<0.4]
 return(sum(h*W)/sum(w)) ### integral value
 }
 meth2(1000) ### carry out the method
